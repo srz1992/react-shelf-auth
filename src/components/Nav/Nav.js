480a82d07b3536e3604663059ea52abf4315c8e9
Shannon Engstrom <shannonengstrom1@gmail.com>
2018-07-05 16:51:38 -0500

    finished add page

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -15,6 +15,11 @@ const Nav = () => (
             Info Page
           </Link>
         </li>
+        <li>
+          <Link to="/add">
+            Add Page
+          </Link>
+        </li>
       </ul>
     </div>
   </div>
