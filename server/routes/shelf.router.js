1fd5daf6c99e4abe9cc77868a4c0142bdd615bad
sean zilka <zilka.sr@gmail.com>
2018-07-05 16:39:49 -0500

    got table. gridlist not working >:/

--- server/routes/shelf.router.js
+++ server/routes/shelf.router.js
@@ -6,7 +6,23 @@ const router = express.Router();
  * Get all of the items on the shelf
  */
 router.get('/', (req, res) => {
-    res.sendStatus(200); // For testing only, can be removed
+    console.log('in /shelf GET route');
+    console.log('authenticated:', req.isAuthenticated());
+    console.log('user', req.user);
+    
+    if(req.isAuthenticated()){
+        let queryText = `SELECT * FROM item`;
+        pool.query(queryText).then((result) =>{
+            console.log('result.rows:', result.rows);
+            res.send(result.rows)
+        }).catch((error)=>{
+            console.log('error!:', error);
+            res.sendStatus(500);
+        })
+    }
+    else {
+        res.sendStatus(403);
+    }    
 });
 
 
--- src/components/UserPage/UserPage.js
+++ src/components/UserPage/UserPage.js
@@ -6,13 +6,36 @@ import Nav from '../../components/Nav/Nav';
 import { fetchUser } from '../../redux/actions/userActions';
 import { triggerLogout } from '../../redux/actions/loginActions';
 
+import axios from 'axios';
+
+//material ui
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import GridList from '@material-ui/core/GridList';
+import GridListTile from '@material-ui/core/GridListTile';
+
+import './UserPage.css'
+
 const mapStateToProps = state => ({
   user: state.user,
 });
 
 class UserPage extends Component {
+  
+  constructor(props){
+    super(props);
+    this.state = {
+      shelf: []
+    }
+  }
+
   componentDidMount() {
     this.props.dispatch(fetchUser());
+    this.getShelfItems();
   }
 
   componentDidUpdate() {
@@ -26,6 +49,23 @@ class UserPage extends Component {
     // this.props.history.push('home');
   }
 
+  getShelfItems = () =>{
+    console.log('getShelfItems');
+    axios.get('/api/shelf')
+    .then((response)=>{
+      console.log('this works');
+      
+      console.log('response.data:', response);
+      this.setState({shelf: [...response.data]})
+      console.log('this.state:', this.state);
+      
+    }).catch((error)=>{
+      console.log('error getting shelf items! ask Bill what is wrong');
+    })
+    ;
+    
+  }
+
   render() {
     let content = null;
 
@@ -37,6 +77,33 @@ class UserPage extends Component {
           >
             Welcome, { this.props.user.userName }!
           </h1>
+          <GridList padding={0} margin={0} className="gridList" cellHeight={160} cols={4}>
+          {this.state.shelf.map((item)=> 
+              <GridListTile key={item.id} cols={2}><img alt={item.description} src={item.image_url} /></GridListTile>
+            )}
+          </GridList>
+          <Paper>
+            <h4>Here is what is on the shelf:</h4>
+          
+         
+          
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell>description</TableCell>
+                <TableCell>image_url</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+            {this.state.shelf.map((item)=> <TableRow key={item.id}>
+              <TableCell>{item.description}</TableCell>
+              <TableCell><img alt={item.description} src={item.image_url} /></TableCell>
+            </TableRow>)}
+            </TableBody>
+          </Table>
+
+          </Paper>
+          
           <button
             onClick={this.logout}
           >
